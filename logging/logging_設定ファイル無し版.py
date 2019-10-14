b384b410db17a299ac604bbd139bd7544364850b
yutaka <yutaka.tanoue@gmail.com>
2019-10-14 14:56:33 +0900

    新規登録

---
+++ logging/logging_設定ファイル無し版.py
@@ -0,0 +1,46 @@
+# -*- coding:utf-8 -*-
+import logging
+
+# level: 指定レベル以上を出力
+# filename: 出力先ファイル
+# asctime: 実行時間
+# lineno: メッセージ出力箇所(行番号)
+# message: メッセージ
+# name: __name__ の内容が入る
+# https://docs.python.org/ja/3/library/logging.html
+
+
+# logging.basicConfig(level=logging.WARNING)
+# logging.basicConfig(filename='test.log', level=logging.WARNING)
+
+formatter = '%(asctime)s %(levelname)s: %(lineno)d: %(message)s'
+logging.basicConfig(level=logging.WARNING, format=formatter)
+
+logging.critical('critical')
+logging.error('error')
+logging.warning('warning')
+logging.info('info')
+# 上でWARNING以上と設定している為、出力されない
+logging.debug('debug')
+
+# ----------------------------------------------------------------------------
+
+# ロガー作成
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# ハンドラ作成
+h = logging.FileHandler('xxx.log')
+
+# フォーマッター作成
+formatter = logging.Formatter('%(asctime)s - %(name)s - '
+                              '%(levelname)s - %(message)s')
+# フォーマッターをハンドラにセット
+h.setFormatter(formatter)
+
+# ハンドラとフォーマッタをロガーに登録
+logger.addHandler(h)
+
+# 設定変更したので出力される(但し、出力先はファイルになる)
+logger.debug('debug')
+
